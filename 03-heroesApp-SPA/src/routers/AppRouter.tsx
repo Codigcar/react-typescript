e5ac654f5711ac090eb27dc45b93d7351c6122ef
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-06-22 06:27:16 -0500

    03 - Added config para manejar las RUTAS

---
+++ 03-heroesApp-SPA/src/routers/AppRouter.tsx
@@ -0,0 +1,18 @@
+import { Navbar } from "../components/ui/Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { LoginScreen } from "../components/login/LoginScreen";
+import { MarvelScreen } from "../components/marvel/MarvelScreen";
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+            <Route exact path="/login" component={LoginScreen} />
+            <Route exact path="/" component={MarvelScreen} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
