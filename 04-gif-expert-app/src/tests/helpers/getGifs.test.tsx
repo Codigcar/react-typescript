e4b17b9db563fc568e4feb7e3784c8b1de71b081
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-07-13 23:30:42 -0500

    04 Test - Pruebas al func getGifs, pruebas que trae 10 elementos y 0 elementos ( gifs.length)

---
+++ 04-gif-expert-app/src/tests/helpers/getGifs.test.tsx
@@ -0,0 +1,20 @@
+
+
+import 'jest';
+import { getGifs } from '../../helpers/getGifs';
+
+
+describe('Pruebas con getGifs Fecth', () => {
+    test('debe de traer 10 elementos', async() => {
+        const gifs = await getGifs('Naruto');
+        expect(gifs.length).toBe(10);
+    })
+
+    test('debe de traer 0 elementos', async() => {
+        const gifs = await getGifs('');
+        expect(gifs.length).toBe(0);
+    })
+    
+    
+    
+})
