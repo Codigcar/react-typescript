5ab6f694c36a3ca3b62d430b1d57038af9d6e860
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-07-13 22:51:09 -0500

    04 Test - Pruebas al <Gif /> con el Title y Url

--- 04-gif-expert-app/src/components/Gif.tsx
+++ 04-gif-expert-app/src/components/Gif.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-export const Gif = (props:any) => {
+export const Gif = ({title, url}:any) => {
     return (
         <div className="card">
-            <img src={props.url} alt="props.title" />
-            <p>{props.title}</p>
+            <img src={url} alt="props.title" />
+            <p>{title}</p>
         </div>
     )
+}
+
+Gif.propTypes = {
+    url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
 }
--- 04-gif-expert-app/src/tests/components/Gif.test.tsx
+++ 04-gif-expert-app/src/tests/components/Gif.test.tsx
@@ -1,12 +1,20 @@
 import { shallow } from 'enzyme';
 import 'jest';
 import { Gif } from '../components/Gif';
-import { useFetchGifs } from '../hooks/useFetchGifs';
+
 describe('Pruebas a <Gif />', () => {
     test('debe mostrar el componente <Gif /> renderizado', () => {
         // const {data, loading}  = useFetchGifs('goku');
         // const gif = data[0];
-        const wrapper = shallow( <Gif />);
+        const dataExample = {
+            title : 'titulo',
+            url : 'https://localhost/algo.jpg'
+        }
+        console.log(dataExample);
+        
+        const title = 'titulo';
+        const url = 'https://localhost/algo.jpg:';
+        const wrapper = shallow( <Gif title={title} url={url} />);
         expect(wrapper).toMatchSnapshot();
         
     })
