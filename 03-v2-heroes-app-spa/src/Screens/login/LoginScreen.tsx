0e7dd16360e5fa7cd91d16e5bcf5213d2867e91d
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-07-23 02:18:31 -0500

    03-v2 - Recordar la ultima vista (Path) visitado x el usuario //comentario

--- 03-v2-heroes-app-spa/src/Screens/login/LoginScreen.tsx
+++ 03-v2-heroes-app-spa/src/Screens/login/LoginScreen.tsx
@@ -6,6 +6,7 @@ export const LoginScreen = ({history}:any) => {
     
     const {signIn, authState} = useContext(AuthContext);
 
+    // Si hay algo guardado en el localStorage que lo retorne, si no hay nada que retorne un '/
     const lastPath = localStorage.getItem('lastPath') || '/';
 
     const handleSignIn = () => {
