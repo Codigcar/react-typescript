c5c466453cd904d579df54257f275f46855fdf17
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-07-25 17:46:27 -0500

    05 - Added TailwindCSS

--- 05-journal-app/src/index.tsx
+++ 05-journal-app/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import './assets/main.css'
+
 import { App } from './App';
 
 ReactDOM.render(
