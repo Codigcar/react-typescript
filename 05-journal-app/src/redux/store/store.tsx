64c0de8aad5ff888880dc43eb511448d4a6bab28
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-07-28 18:14:49 -0500

    05 - Redux DevTools

--- 05-journal-app/src/redux/store/store.tsx
+++ 05-journal-app/src/redux/store/store.tsx
@@ -1,8 +1,21 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, compose, createStore } from "redux";
 import { authReducer } from '../reducers/authReducer';
+
+// Configuración consola Redux
+declare global {
+    interface Window {
+      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+  }
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// end Configuración consola Redux
 
 const reducers = combineReducers({
     auth: authReducer
 })
 
-export const store = createStore(reducers);
+export const store = createStore(
+    reducers,
+    composeEnhancers()
+);
