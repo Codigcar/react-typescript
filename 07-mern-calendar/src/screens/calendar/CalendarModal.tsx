283650e72ffa7026a026dd17ac236305c297c8f7
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-08-07 21:55:33 -0500

    07 - Estilos al Modal, fondo más gris (overlay) y closeTimeout de 2sg

--- 07-mern-calendar/src/screens/calendar/CalendarModal.tsx
+++ 07-mern-calendar/src/screens/calendar/CalendarModal.tsx
@@ -6,13 +6,12 @@ import Modal from 'react-modal';
 
 const customStyles = {
     content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-    
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
     },
   };
 
@@ -29,12 +28,16 @@ export const CalendarModal = () => {
     }
 
     return (
-        <Modal
+        <Modal 
         isOpen={isOpen}
         // onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
-        style={customStyles}
+        // style={customStyles}
         contentLabel="Example Modal"
+
+        className="modal" 
+        overlayClassName="modal-fondo"
+        closeTimeoutMS={200}
         >
             <h1>Hola Modal</h1>
             <hr />
