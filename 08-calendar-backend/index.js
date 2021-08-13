67b036a6708717c5af4e640c2796ba84238d7347
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-08-13 12:41:34 -0500

    08 - Instalando Express y configurando primera Ruta 'app.get(/)' con port:4000

--- 08-calendar-backend/index.js
+++ 08-calendar-backend/index.js
@@ -1,1 +1,19 @@
-console.log('holassss');
+const express = require("express");
+
+// import express from "express";
+
+// Crear el servidor de express
+const app = express();
+
+// Rutas
+app.get("/", (req, res) => {
+  console.log("se requiere /");
+  res.json({
+      ok:true
+  })
+});
+
+// Escuchar peticiones
+app.listen(4000, () => {
+  console.log(`servidor corriendo en el puerto ${4000}`);
+});
