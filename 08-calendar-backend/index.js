5695adf1137a7667aafe6694963e4e981166398d
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-08-13 12:59:42 -0500

    08 - Variables de entorno y carpeta publica (html y css)

--- 08-calendar-backend/index.js
+++ 08-calendar-backend/index.js
@@ -1,19 +1,25 @@
 const express = require("express");
-
 // import express from "express";
+
+// traer variables de entornos
+require('dotenv').config();
+console.log(process.env);
 
 // Crear el servidor de express
 const app = express();
 
 // Rutas
-app.get("/", (req, res) => {
-  console.log("se requiere /");
-  res.json({
-      ok:true
-  })
-});
+// app.get("/", (req, res) => {
+//   console.log("se requiere /");
+//   res.json({
+//       ok:true
+//   })
+// });
+
+// Reemplazo de 'Rutas' x un Directorio Público
+app.use(express.static('public'));
 
 // Escuchar peticiones
-app.listen(4000, () => {
-  console.log(`servidor corriendo en el puerto ${4000}`);
+app.listen(process.env.PORT, () => {
+  console.log(`servidor corriendo en el puerto ${process.env.PORT}`);
 });
