fa4d196f7535146b0ecac9bd366ab9584c49bcbb
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-08-14 12:50:59 -0500

    08 - Crear un usuario en la BD Mongo con el ODM Mongoose

--- 08-calendar-backend/controllers/auth_controller.js
+++ 08-calendar-backend/controllers/auth_controller.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
+const Usuario = require("../models/usuario_model");
 
-const crearUsuario = (req,res = express.response) => {
-
-    const {name, email, password} = req.body;
+const crearUsuario = async (req, res = express.response) => {
+  try {
+    const { name, email, password } = req.body;
     // console.log(req);
+
+    const usuario = new Usuario(req.body);
+    await usuario.save();
 
     // if ( name.length < 5 ) {
     //     return res.status(400).json({
@@ -12,37 +16,43 @@ const crearUsuario = (req,res = express.response) => {
     //     })
     // }
 
-
     res.status(201).json({
-        ok:true,
-        msg: 'registro22',
-        name: name,
-        email: email,
-        password, password
+      ok: true,
+      msg: "registro22",
+      name: name,
+      email: email,
+      password,
+      password,
+    });
+  } catch (error) {
+      console.log('Error /new: ', error);
+      res.status(500).json({
+          ok: false,
+          msg: 'Contactar con el admin'
       });
-}
+  }
+};
 
 const loginUsuario = (req, res = express.response) => {
-    const {email, password} = req.body;
+  const { email, password } = req.body;
 
-    res.json({
-        ok:true,
-        msg: 'login',
-        email,
-        password
-    })
-}
+  res.json({
+    ok: true,
+    msg: "login",
+    email,
+    password,
+  });
+};
 
 const revalidarToken = (req, resp = express.response) => {
-    res.json({
-        ok:true,
-        msg:'renew'
-    })
-}
+  res.json({
+    ok: true,
+    msg: "renew",
+  });
+};
 
-
-  module.exports = {
-      crearUsuario,
-      loginUsuario,
-      revalidarToken
-  }
+module.exports = {
+  crearUsuario,
+  loginUsuario,
+  revalidarToken,
+};
