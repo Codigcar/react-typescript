dc01c8b1979698f88ee86f1e56721c484839ecde
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-08-14 12:16:58 -0500

    08 - Conectar Node a Mongo Atlas con carpeta Database y .env conection

--- 08-calendar-backend/index.js
+++ 08-calendar-backend/index.js
@@ -1,12 +1,17 @@
-const express = require("express");
 // import express from "express";
-
+const express = require("express");
 // traer variables de entornos
 require('dotenv').config();
+const { dbConnection } = require("./database/config");
+
 // console.log(process.env);
 
 // Crear el servidor de express
 const app = express();
+
+// conexión Base de datos con MongoBD Atlas
+dbConnection();
+
 
 // Directorio pubico
 // Reemplazo de 'Rutas' x un Directorio Público
