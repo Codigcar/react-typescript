7ee88ab4f99ae1f32235581871a6b57d08752b85
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-08-22 00:15:14 -0500

    10 -  Registrar usuario y Mostrar erro con SwalAlert

--- 10-mern-calendar-backend-auth/src/redux/actions/authActions.tsx
+++ 10-mern-calendar-backend-auth/src/redux/actions/authActions.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import Swal from 'sweetalert2';
 import { fetchSinToken } from '../../helpers/fetch';
 import { actionTypes } from '../types/type';
 
 export const startLoginAction = (email: string, password: string) => {
 
     return async (dispatch:any) => {
-        console.log(email, password);
+        console.log('Datos Login: ', email, password);
         const resp = await fetchSinToken('auth', { email, password }, 'POST');
         const body = await resp.json();
 
-        console.log('back resp: ', body);
+        console.log('back login resp: ', body);
 
         if (body.ok) {
             localStorage.setItem('token', body.token);
@@ -17,8 +18,32 @@ export const startLoginAction = (email: string, password: string) => {
             localStorage.setItem('token-init-date', new Date().getTime().toString());
 
             dispatch(login({uid: body.uid, name: body.name}));
+        } else {
+            Swal.fire('Error', body.msg, 'error')
         }
     }
+}
+
+
+export const startRegister = (email:string, password: string, name:string ) => {
+    return async(dispatch: any) => {
+        console.log('Datos Register: ', email, password, name);
+        const resp = await fetchSinToken('auth/new', {email, password, name}, 'POST');
+        const body = await resp.json();
+        
+        console.log('back register resp: ', body);
+        
+        if (body.ok){
+            localStorage.setItem('token', body.token);
+            localStorage.setItem('token-init-date', new Date().getTime().toString());
+            
+            dispatch(login({uid: body.uid, name: body.name}));
+        } else {
+            Swal.fire('Error', body.msg, 'error');
+        }
+        
+    }
+
 }
 
 const login = (user: any) => {
@@ -27,3 +52,11 @@ const login = (user: any) => {
         payload: user
     }
 }
+
+const register = (user:any) => {
+    return {
+
+    }
+}
+
+
--- 10-mern-calendar-backend-auth/src/screens/login/LoginScreen.tsx
+++ 10-mern-calendar-backend-auth/src/screens/login/LoginScreen.tsx
@@ -1,29 +1,59 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { useForm } from '../../hooks/useForm'
-import { startLoginAction } from '../../redux/actions/authActions';
+import { startLoginAction, startRegister } from '../../redux/actions/authActions';
 
-interface FormData {
+interface FormDataLogin {
     loginEmail: string,
     loginPasswod: string
+}
+
+interface FormDataRegister {
+    registerName: string,
+    registerEmail: string,
+    registerPassword1: string,
+    registerPassword2: string
 }
 
 export const LoginScreen = () => {
 
     const dispatch = useDispatch();
 
-    const { formulario, handleChange } = useForm<FormData>({
+    // Login
+    const { formulario, handleChange } = useForm<FormDataLogin>({
         loginEmail: 'carlos@gmail.com',
         loginPasswod: '123123'
     });
 
     const { loginEmail, loginPasswod } = formulario;
 
-    const handleLogin = (e:any) => {
+    const handleLogin = (e: any) => {
         e.preventDefault();
         // console.log('formulario: ',formulario);
         dispatch(startLoginAction(loginEmail, loginPasswod));
     }
+
+    // --Login
+
+    // Register
+    const { formulario: { registerName, registerEmail, registerPassword1, registerPassword2 }, handleChange: handleInputRegisterChange } = useForm<FormDataRegister>({
+        registerName: 'Estefanny',
+        registerEmail: 'estefanny@gmail.com',
+        registerPassword1: '123123',
+        registerPassword2: '123123',
+    })
+   
+
+   
+    const handleRegisterSubmit = (e: any) => {
+        e.preventDefault();
+        // console.log('formulario: ',formulario);
+        dispatch(startRegister(registerEmail, registerPassword1, registerName));
+        // console.log('hola');
+        
+    }
+    // --Register
+
 
     return (
         <div >
@@ -43,15 +73,33 @@ export const LoginScreen = () => {
                     </div>
                     <div className="flex flex-col flex-1 bg-blue-600 rounded-r-xl ">
                         <h1 className="my-16 text-3xl font-bold text-center text-white ">Register</h1>
-                        <div className="flex flex-col items-center justify-start flex-1">
-                            <input type="text" className="w-3/4 p-1 mb-3 border-2 rounded focus:outline-none focus:border-blue-600" placeholder="Nombre" />
-                            <input type="text" className="w-3/4 p-1 mb-3 border-2 rounded focus:outline-none focus:border-blue-600" placeholder="Correo" />
-                            <input type="text" className="w-3/4 p-1 mb-3 border-2 rounded focus:outline-none focus:border-blue-600" placeholder="Contraseña" />
-                            <input type="text" className="w-3/4 p-1 mb-3 border-2 rounded focus:outline-none focus:border-blue-600" placeholder="Repite contraseña" />
-                            <div className="w-3/4 border border-none ">
-                                <p className="w-1/3 p-1 text-center bg-white rounded cursor-pointer">Crear cuenta</p>
+                        <form onSubmit={handleRegisterSubmit}>
+                            <div className="flex flex-col items-center justify-start flex-1">
+                                <input type="text" className="w-3/4 p-1 mb-3 border-2 rounded focus:outline-none focus:border-blue-600" placeholder="Nombre"
+                                    name="registerName"
+                                    value={registerName}
+                                    onChange={handleInputRegisterChange}
+                                />
+                                <input type="text" className="w-3/4 p-1 mb-3 border-2 rounded focus:outline-none focus:border-blue-600" placeholder="Correo"
+                                    name="registerEmail"
+                                    value={registerEmail}
+                                    onChange={handleInputRegisterChange}
+                                />
+                                <input type="text" className="w-3/4 p-1 mb-3 border-2 rounded focus:outline-none focus:border-blue-600" placeholder="Contraseña"
+                                    name="registerPassword1"
+                                    value={registerPassword1}
+                                    onChange={handleInputRegisterChange}
+                                />
+                                <input type="text" className="w-3/4 p-1 mb-3 border-2 rounded focus:outline-none focus:border-blue-600" placeholder="Repite contraseña"
+                                    name="registerPassword2"
+                                    value={registerPassword2}
+                                    onChange={handleInputRegisterChange}
+                                />
+                                <button type="submit" className="w-3/4 border border-none ">
+                                    <p className="w-1/3 p-1 text-center bg-white rounded cursor-pointer">Crear cuenta</p>
+                                </button>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
