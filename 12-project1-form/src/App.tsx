bee25d037b67888fd2c4c6dc7eaefc808a753afa
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-08-30 16:50:36 -0500

    12 - Config DarkMode

--- 12-project1-form/src/App.tsx
+++ 12-project1-form/src/App.tsx
@@ -1,14 +1,34 @@
-import { CssBaseline } from '@material-ui/core'
+import { CssBaseline, Switch } from '@material-ui/core'
 import { HomeView } from './views/HomeView';
 import './App.scss';
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from './ThemeConfig';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import { useState } from 'react';
+// import theme from './ThemeConfig';
 
 export const App = () => {
+  
+  const [modeDark, setModeDark] = useState(false);
+
+
+  const theme = createTheme({
+    palette: {
+      type: modeDark? "dark" : "light",
+      primary:{
+        main: '#FFCA28'
+      }
+    }
+  });
+
+  console.log('dark: ', modeDark);
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+
+      <Switch 
+        onClick={() => setModeDark(!modeDark) }
+      />
+
       <div className="container">
         <HomeView />
       </div>
